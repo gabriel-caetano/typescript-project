95d5a6581aeb08f80ad0436e7d77f9247bcc739b
Gabriel Caetano <gvcaetano@inf.ufsm.br>
2022-03-08 19:00:01 -0300

    part: trying to work with knex

--- src/knexfile.ts
+++ src/knexfile.ts
@@ -1,37 +1,18 @@
 import type { Knex } from 'knex'
+import dotenv from 'dotenv'
+dotenv.config()
 
 // Update with your config settings.
 
 const config: { [key: string]: Knex.Config } = {
   development: {
-    client: 'sqlite3',
+    client: process.env.DB_CONNECTION,
     connection: {
-      filename: './dev.sqlite3',
-    },
-  },
-
-  staging: {
-    client: 'postgresql',
-    connection: {
-      database: 'my_db',
-      user: 'username',
-      password: 'password',
-    },
-    pool: {
-      min: 2,
-      max: 10,
-    },
-    migrations: {
-      tableName: 'knex_migrations',
-    },
-  },
-
-  production: {
-    client: 'postgresql',
-    connection: {
-      database: 'my_db',
-      user: 'username',
-      password: 'password',
+      host: process.env.MYSQL_HOST,
+      port: Number(process.env.MYSQL_PORT),
+      user: process.env.MYSQL_USER,
+      password: process.env.MYSQL_PASSWORD,
+      database: process.env.MYSQL_DB_NAME,
     },
     pool: {
       min: 2,
@@ -44,3 +25,4 @@ const config: { [key: string]: Knex.Config } = {
 }
 
 module.exports = config
+export default config
--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,13 @@
+import dotenv from 'dotenv'
+dotenv.config()
 import express from 'express'
 import { adminsRoutes } from './routes/admin.routes'
+
 const app = express()
+
+const port = process.env.PORT || 3333
 
 app.use(express.json())
 app.use('/admin', adminsRoutes)
 
-app.listen(3000, () => console.log('running...'))
+app.listen(port, () => console.log(`running server on port ${port}...`))
